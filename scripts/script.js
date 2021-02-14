70beab92dc6183304cfb738a3efebf13f9a33cbd
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-02-14 10:17:55 -0500

    removed form validation

--- scripts/script.js
+++ scripts/script.js
@@ -39,96 +39,96 @@ function closeMobileMenu() {
     screenBlur.classList.remove('blur-screen');
 }
 
-// --------------- Contact form validation
-const form = document.getElementById('contact-form');
-const nameField = document.getElementById('name');
-const emailField = document.getElementById('email');
-const messageField = document.getElementById('message');
-const messageDiv = document.getElementById('message-form');
+// // --------------- Contact form validation
+// const form = document.getElementById('contact-form');
+// const nameField = document.getElementById('name');
+// const emailField = document.getElementById('email');
+// const messageField = document.getElementById('message');
+// const messageDiv = document.getElementById('message-form');
 
-form.addEventListener('submit', checkFields);
+// form.addEventListener('submit', checkFields);
 
-// User input error handling 
-function checkFields(e) {
-    e.preventDefault();
+// // User input error handling 
+// function checkFields(e) {
+//     e.preventDefault();
 
-    const name = nameField.value;
-    const email = emailField.value;
-    const message = messageField.value;
+//     const name = nameField.value;
+//     const email = emailField.value;
+//     const message = messageField.value;
 
-    const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     
-    // All errors found are pushed in an array
-    var errorMessage = [];
+//     // All errors found are pushed in an array
+//     var errorMessage = [];
 
-    if (name === '') {
-        errorMessage.push('Please enter a name.');
-    }
-    if (name.length < 3) {
-        errorMessage.push('Name must be at least 3 characters.');
-    }
-    if (!regexEmail.test(email)) {
-        errorMessage.push('Email must match this format: youremail@email.com.');
-    }
-    if (message === '' || message.length < 10) {
-        errorMessage.push('Message must be at least 10 characters long.');
-    }
+//     if (name === '') {
+//         errorMessage.push('Please enter a name.');
+//     }
+//     if (name.length < 3) {
+//         errorMessage.push('Name must be at least 3 characters.');
+//     }
+//     if (!regexEmail.test(email)) {
+//         errorMessage.push('Email must match this format: youremail@email.com.');
+//     }
+//     if (message === '' || message.length < 10) {
+//         errorMessage.push('Message must be at least 10 characters long.');
+//     }
 
-    // If the errors array has errors stored, then display the errors
-    if (errorMessage.length > 0) {
-        displayErrorMessage(errorMessage);
-    }
-    // Otherwise display the 'sent' message
-    // else {
-    //     displaySentMessage();
-    // }
-    const bodyMessage = name + '+' + email + '+' + message;
-    const options = {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: bodyMessage,
-    }
+//     // If the errors array has errors stored, then display the errors
+//     if (errorMessage.length > 0) {
+//         displayErrorMessage(errorMessage);
+//     }
+//     // Otherwise display the 'sent' message
+//     // else {
+//     //     displaySentMessage();
+//     // }
+//     const bodyMessage = name + '+' + email + '+' + message;
+//     const options = {
+//         method: "POST",
+//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//         body: bodyMessage,
+//     }
 
-    fetch('/', options)
-    .then(response => displaySentMessage())
-    .catch(error => console.log('error', error))
-}
+//     fetch('/', options)
+//     .then(response => displaySentMessage())
+//     .catch(error => console.log('error', error))
+// }
 
-// Loops through the errors array and displays all of them
-function displayErrorMessage(messageArray) {
-    messageArray.map(message => {
-        messageDiv.innerText += message + " ";
-    })
+// // Loops through the errors array and displays all of them
+// function displayErrorMessage(messageArray) {
+//     messageArray.map(message => {
+//         messageDiv.innerText += message + " ";
+//     })
 
-    messageDiv.classList.toggle('error');
+//     messageDiv.classList.toggle('error');
 
-    setTimeout(() => {
-        messageDiv.classList.toggle('error');
-        clearMessages()
-    }, 3000);
-}
+//     setTimeout(() => {
+//         messageDiv.classList.toggle('error');
+//         clearMessages()
+//     }, 3000);
+// }
 
-// Displays a 'sent' message
-function displaySentMessage() {
-    messageDiv.innerText = 'Thank you! I\'ll reply as soon as I can.';
-    messageDiv.classList.toggle('sent');
+// // Displays a 'sent' message
+// function displaySentMessage() {
+//     messageDiv.innerText = 'Thank you! I\'ll reply as soon as I can.';
+//     messageDiv.classList.toggle('sent');
 
-    nameField.value = '';
-    emailField.value = '';
-    messageField.value = '';
+//     nameField.value = '';
+//     emailField.value = '';
+//     messageField.value = '';
 
-    setTimeout(() => {
-        messageDiv.classList.toggle('sent');
-        clearMessages();
-    }, 3000);
-}
+//     setTimeout(() => {
+//         messageDiv.classList.toggle('sent');
+//         clearMessages();
+//     }, 3000);
+// }
 
-// Clears the div element to display other messages
-function clearMessages() {
-    while(messageDiv.firstChild) {
-        messageDiv.removeChild(messageDiv.firstChild);
-    }
-}
+// // Clears the div element to display other messages
+// function clearMessages() {
+//     while(messageDiv.firstChild) {
+//         messageDiv.removeChild(messageDiv.firstChild);
+//     }
+// }
 
 // ----------------- Home page animation
 // Randomize all the numbers for CSS position and animation duration for small and medium dots
